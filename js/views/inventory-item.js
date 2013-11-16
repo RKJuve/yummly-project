4f0feb413aebf749d1b7ed5ba42bfac261a00271
Timothy Malcham <timothymalcham@gmail.com>
2013-11-16 15:59:53 -0800

    random untracked files I missed

---
+++ js/views/inventory-item.js
@@ -0,0 +1,10 @@
+APP.InventoryItemView = Backbone.View.extend({
+  tagName: "li",
+  className: "list-item",
+  template: Handlebars.compile("<a>{{ name }}</a>"),
+  render: function() {
+    var attributes = this.model.toJSON();
+    this.$el.html(this.template(attributes));
+    return this;
+  }
+});
