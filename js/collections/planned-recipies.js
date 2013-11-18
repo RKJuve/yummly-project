fd0429ed93b11ab008270d441f7745a1e49a1c4b
Ryan Juve <RKJuve@gmail.com>
2013-11-17 17:16:18 -0800

    ingredient lines are automatically muxed when added to plannedRecipes

--- js/collections/planned-recipies.js
+++ js/collections/planned-recipies.js
@@ -1,3 +1,11 @@
 APP.PlannedRecipies = Backbone.Collection.extend({
-	model: APP.PlannedRecipie
+	model: APP.RecipieDetails,
+	initialize: function() {
+		this.bindEvents();
+	},
+	bindEvents: function() {
+		this.on('add', function(model) {
+			model.mux();
+		});
+	}
 });
