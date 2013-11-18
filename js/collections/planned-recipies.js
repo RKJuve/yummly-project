52330143859051e54684d12824dcd220fe5bc974
Ryan Juve <RKJuve@gmail.com>
2013-11-17 17:30:48 -0800

    shopping list now automatically created/updated on saving to plannedRecipes

--- js/collections/planned-recipies.js
+++ js/collections/planned-recipies.js
@@ -6,6 +6,12 @@ APP.PlannedRecipies = Backbone.Collection.extend({
 	bindEvents: function() {
 		this.on('add', function(model) {
 			model.mux();
+			if (!APP.shoppingList) {
+				APP.shoppingList = new APP.ShoppingList;
+			}
+			model.attributes.ingredientLines.forEach(function(el, index, array){
+				APP.shoppingList.add(new APP.ShoppingListItem(el));
+			});
 		});
 	}
 });
