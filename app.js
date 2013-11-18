d5ed815320a0270e1555a7fe92a76f9af3b1c610
Timothy Malcham <timothymalcham@gmail.com>
2013-11-18 15:27:04 -0800

    starting to set up login screen

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 var path = require("path"),
 		express = require("express"),
-		_ = require("underscore");
+		_ = require("underscore"),
+		Dropbox = require("dropbox");
 
 //set up the express app
 var app = express()
@@ -11,52 +12,45 @@ var app = express()
 	.use(express.static(__dirname))
 	.use(express.bodyParser());
 
-
-
 var db = [
 	{
-		id: 1,
 		name: "flour"
 	},
 	{
-		id: 2,
 		name: "milk"
 	},
 	{
-		id: 3,
 		name: "eggs"
 	}
 ];
 
 var weekdb = [
 	{
-		id: 1,
 		day: "Monday"
 	},
 	{
-		id: 2,
 		day: "Tuesday"
 	},
 	{
-		id: 3,
 		day: "Saturday"
 	}
 ];
 
 var favoritesdb = [
 	{
-		id: 1,
 		name: "Favorite Recipe 1"
 	},
 	{
-		id: 2,
 		name: "Favorite Recipe 2"
 	},
 	{
-		id: 3,
 		name: "Favorite Recipe 3"
 	}
 ];
+
+ var getId = function() {
+   return id++;
+ };
 
 
 app.get("/inventory", function(req, res) {
@@ -86,7 +80,7 @@ app.get("/week", function(req, res) {
 });
 
 app.post("/week", function(req, res) {
-	//req.body.id = db.length + 1;
+	req.body.id = db.length + 1;
 	weekdb.push(req.body);
 	res.end();
 	console.log(id);
@@ -97,7 +91,8 @@ app.get("/favorites", function(req, res) {
 });
 
 app.post("/favorites", function(req, res) {
-	//req.body.id = db.length + 1;
+	req.body.id = favoritesdb.length + 1;
+	getId();
 	favoritesdb.push(req.body);
 	res.end();
 	console.log(id);
