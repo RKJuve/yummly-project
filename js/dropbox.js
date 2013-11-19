1b6f76a80fdfffb5b18376910d4fe906b3d2fa61
Timothy Malcham <timothymalcham@gmail.com>
2013-11-19 08:19:50 -0800

    added simple welcome screen that obscures app when you haven't logged in or signed up

--- js/dropbox.js
+++ js/dropbox.js
@@ -1,21 +1,29 @@
 var client = new Dropbox.Client({key: "i401wu5aqq6zpwk"});
 
 
-$(".login").click(function(e) {
-	e.preventDefault();
-	client.authenticate();
+$(document).ready(function() {
+
+	$(".container").hide();
+	$("html").css("background-color", "#a9e160");
+
+	$(".login").click(function(e) {
+		e.preventDefault();
+		client.authenticate();
+	});
+
+	// Try to finish OAuth authorization.
+	client.authenticate({interactive: false}, function (error) {
+	  if (error) {
+	    alert('Authentication error: ' + error);
+	  }
+	});
+
+	if (client.isAuthenticated()) {
+	  console.log('auth sucessful');
+	  $(".login").hide();
+	  $(".welcome").hide();
+	  $("html").css("background-color", "#F3F3F3");
+	  $(".container").show();
+	}
+
 });
-
-// Try to finish OAuth authorization.
-client.authenticate({interactive: false}, function (error) {
-  if (error) {
-    alert('Authentication error: ' + error);
-  }
-});
-
-if (client.isAuthenticated()) {
-  console.log('auth sucessful');
-  $(".login").hide();
-  $(".content").show();
-}
-
