bd787a9f4b7ced5b4ba67725ecce7652357e073a
Ryan Juve <RKJuve@gmail.com>
2013-11-18 16:04:23 -0800

    working on add-user-recipe, also dropbox integration

--- js/views/add-recipe.js
+++ js/views/add-recipe.js
@@ -1,10 +1,18 @@
 APP.AddRecipeView = Backbone.View.extend({
 	el: "#bin",
+	events: {
+		"submit form": "submit"
+	},
 	initialize: function() {
 		this.render();
 	},
+	submit: function() {
+		console.log('form submit');
+		return false;
+	},
 	render: function() {
-		console.log("add recipe render");
-		this.$el.html('<form><input type="text"></input><input type="text"></input><input type="text"></input></form>');
+		var source = $("#user-recipe-form").html();
+		template = Handlebars.compile(source); 
+		this.$el.html(template);
 	}
 });
